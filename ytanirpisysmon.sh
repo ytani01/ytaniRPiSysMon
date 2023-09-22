5764f3c4ac5589f37221061264c7dd417692fb91
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2023-09-22 22:44:55 +0900

    UPD

--- ytanirpisysmon.sh
+++ ytanirpisysmon.sh
@@ -3,10 +3,17 @@
 # Copyright (c) 2023 Yoichi Tanibayashi. All rights reserved.
 #
 
-INTERVAL_SEC=3
+INTERVAL_SEC=0
+if [ $# -gt 0 ]; then
+    expr $1 + 1 > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+       	INTERVAL_SEC=$1
+    fi
+fi
+#echo "INTERVAL_SEC=$INTERVAL_SEC"
 
-if which bc; then
-    echo ""
+if which bc > /dev/null 2>& 1; then
+    echo -n ""
 else
     sudo apt install -y bc
 fi
@@ -16,8 +23,22 @@ get_vcgencmd_val() {
     echo $RES | sed 's/^.*=//'
 }
 
-hz2ghz() {
-    echo "scale=2; $1 / 1000000000" | bc | sed 's/^\./0./'
+hz2str() {
+    RET_GHZ=`echo "scale=2; $1 / 1000000000" | bc | sed 's/^\./0./'`
+    if [ `echo "$RET_GHZ >= 1" | bc` = 1 ]; then
+	echo $RET_GHZ GHz
+	return 0
+    fi
+
+    RET_MHZ=`echo "$RET_GHZ * 1000" | bc | sed 's/\..*$//'`
+    echo $RET_MHZ MHz
+    return 0
+}
+
+hz2mhz() {
+    RET_MHZ=`echo "scale=0; $1 / 1000000" | bc`
+    echo $RET_MHZ MHz
+    return 0
 }
 
 while true; do
@@ -27,13 +48,18 @@ while true; do
     THROTTLED=`get_vcgencmd_val get_throttled`
 
     HZ_ARM=`get_vcgencmd_val measure_clock arm`
-    GHZ_ARM=`hz2ghz $HZ_ARM`
+    HZ_ARM_STR=`hz2mhz $HZ_ARM`
 
     HZ_CORE=`get_vcgencmd_val measure_clock core`
-    GHZ_CORE=`hz2ghz $HZ_CORE`
+    HZ_CORE_STR=`hz2mhz $HZ_CORE`
 
 
-    echo "CPU: $TEMP_CPU,  ARM: ${GHZ_ARM} GHz,  Core: ${GHZ_CORE} GHz  $VOLTS_CORE V  $THROTTLED"
+    DATE_TIME=`date +'%Y/%m/%d %T'`
 
+    echo "$DATE_TIME  CPU: $TEMP_CPU,  ARM: ${HZ_ARM_STR} ,  Core: ${HZ_CORE_STR}  $VOLTS_CORE V  $THROTTLED"
+
+    if [ $INTERVAL_SEC -eq 0 ]; then
+	break
+    fi
     sleep $INTERVAL_SEC
 done
